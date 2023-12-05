5b1ff2cf575f197184bacf263f822154d55600a6
Tomasz Gieniusz <tomasz.gieniusz@certsy.com>
2023-12-05 18:29:24 +1100

    Fix activerecord 7.1 deprecation warnings

--- spec/que/active_record/connection_spec.rb
+++ spec/que/active_record/connection_spec.rb
@@ -39,7 +39,7 @@ if defined?(::ActiveRecord)
         establish_connection(QUE_URL)
       end
 
-      SecondDatabaseModel.clear_active_connections!
+      SecondDatabaseModel.connection_handler.clear_active_connections!(:all)
       refute SecondDatabaseModel.connection_handler.active_connections?
 
       class SecondDatabaseModelJob < Que::Job
